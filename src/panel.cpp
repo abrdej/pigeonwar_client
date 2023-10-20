c3cc345c70b81997ba2e3d13495dbb4045e4b468
abrdej <abrdej@gmail.com>
2023-10-20 18:22:49 +0200

    Move panel from h to cpp

--- src/panel.cpp
+++ src/panel.cpp
@@ -1,3 +1,49 @@
 #include <panel.h>
 
+#include <texture_loader.h>
 
+#include <iostream>
+
+Panel::Panel(TextureLoader& texture_loader, int pos_x, int pos_y, int number_of_buttons)
+    : texture_loader_(texture_loader), pos_x_(pos_x), pos_y_(pos_y) {
+  buttons_.reserve(number_of_buttons);
+  for (int n = 0; n < number_of_buttons; ++n) {
+    buttons_.emplace_back(texture_loader.GetTexture("border"),
+                          pos_x + n * 60,
+                          pos_y,
+                          60).OnClicked([n](const Button& button) {
+      std::cout << "Button: " << n << " clicked\n";
+    });
+    buttons_order_.emplace_back(ButtonOrderHolder{&buttons_.back()});
+  }
+}
+
+void Panel::Draw(Window& window) {
+  std::sort(std::begin(buttons_order_), std::end(buttons_order_), Compare{});
+  for (auto& button : buttons_order_) {
+    button.button->Draw(window);
+  }
+  if (entity_button_) {
+    entity_button_->Draw(window);
+  }
+}
+
+void Panel::InteractMove(int x, int y) {
+  for (auto& button : buttons_) {
+    button.InteractMove(x, y);
+  }
+}
+
+void Panel::InteractPress(int x, int y) {
+  for (auto& button : buttons_) {
+    button.InteractPress(x, y);
+  }
+}
+
+bool Panel::Clicked(int x, int y) const {
+  return x >= pos_x_ && x <= pos_x_ + buttons_.size() * 60 && y >= pos_y_ && y <= pos_y_ + buttons_.size() * 60;
+}
+
+void Panel::SetEntityButtonTexture(const std::string& texture_key) {
+  entity_button_ = std::make_unique<Button>(texture_loader_.GetTexture(texture_key), 15, 600, 120);
+}
--- src/panel.h
+++ src/panel.h
@@ -2,53 +2,24 @@
 
 #include <button.h>
 
-#include <iostream>
+#include <memory>
+#include <vector>
+
+class TextureLoader;
 
 class Panel {
  public:
-  Panel(TextureLoader& texture_loader, int pos_x, int pos_y, int number_of_buttons)
-      : texture_loader_(texture_loader), pos_x_(pos_x), pos_y_(pos_y) {
-    buttons_.reserve(number_of_buttons);
-    for (int n = 0; n < number_of_buttons; ++n) {
-      buttons_.emplace_back(texture_loader.GetTexture("border"),
-                            pos_x + n * 60,
-                            pos_y,
-                            60).OnClicked([n](const Button& button) {
-        std::cout << "Button: " << n << " clicked\n";
-      });
-      buttons_order_.emplace_back(ButtonOrderHolder{&buttons_.back()});
-    }
-  }
+  Panel(TextureLoader& texture_loader, int pos_x, int pos_y, int number_of_buttons);
 
-  void Draw(Window& window) {
-    std::sort(std::begin(buttons_order_), std::end(buttons_order_), Compare{});
-    for (auto& button : buttons_order_) {
-      button.button->Draw(window);
-    }
-    if (entity_button_) {
-      entity_button_->Draw(window);
-    }
-  }
+  void Draw(Window& window);
 
-  void InteractMove(int x, int y) {
-    for (auto& button : buttons_) {
-      button.InteractMove(x, y);
-    }
-  }
+  void InteractMove(int x, int y);
 
-  void InteractPress(int x, int y) {
-    for (auto& button : buttons_) {
-      button.InteractPress(x, y);
-    }
-  }
+  void InteractPress(int x, int y);
 
-  [[nodiscard]] bool Clicked(int x, int y) const {
-    return x >= pos_x_ && x <= pos_x_ + buttons_.size() * 60 && y >= pos_y_ && y <= pos_y_ + buttons_.size() * 60;
-  }
+  [[nodiscard]] bool Clicked(int x, int y) const;
 
-  void SetEntityButtonTexture(const std::string& texture_key) {
-    entity_button_ = std::make_unique<Button>(texture_loader_.GetTexture(texture_key), 15, 600, 120);
-  }
+  void SetEntityButtonTexture(const std::string& texture_key);
 
  private:
   TextureLoader& texture_loader_;
