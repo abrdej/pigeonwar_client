fdb25978fc662891a33799037dbe36edb77faba8
abrdej <abrdej@gmail.com>
2023-10-27 21:09:43 +0200

    Move board implementation to cpp

--- src/board.cpp
+++ src/board.cpp
@@ -1,3 +1,54 @@
 #include <board.h>
 
+#include <texture_loader.h>
+#include <window.h>
 
+#include <stdexcept>
+
+Board::Board(TextureLoader& texture_loader, int cols, int rows)
+    : texture_loader_(texture_loader), cols_(cols), rows_(rows) {
+  for (int r = 0; r < rows_; ++r) {
+    for (int c = 0; c < cols_; ++c) {
+      fields_.emplace_back(texture_loader_.GetTexture("grass")).SetPos(c * 60, r * 60);
+    }
+  }
+}
+
+void Board::Draw(Window& window) {
+  for (auto& field : fields_) {
+    field.Draw(window);
+  }
+}
+
+bool Board::Clicked(int x, int y) const {
+  return x >= 0 && x <= cols_ * 60 && y >= 0 && y <= rows_ * 60;
+}
+
+void Board::ChangeTexture(int x, int y, const std::string& texture_key) {
+  ChangeTexture(ToIndex(x, y), texture_key);
+}
+
+void Board::ChangeTexture(IndexType index, const std::string& texture_key) {
+  auto [x, y] = TextureAt(index).GetPos();
+  TextureAt(index) = texture_loader_.GetTexture(texture_key);
+  TextureAt(index).SetPos(x, y);
+}
+
+IndexType Board::ToIndex(int x, int y) const {
+  return y * cols_ + x;
+}
+
+void Board::InteractPress(int x, int y) {
+  if (Clicked(x, y)) {
+    // pos to index
+
+  }
+}
+
+Texture& Board::TextureAt(IndexType index) {
+  try {
+    return fields_.at(index);
+  } catch (std::out_of_range&) {
+    throw std::out_of_range("Board::TextureAt: index out of range: " + std::to_string(index));
+  }
+}
--- src/board.h
+++ src/board.h
@@ -1,53 +1,38 @@
 #pragma once
 
+#include <defs.h>
+#include <texture.h>
+
 #include <vector>
 
-#include <window.h>
-#include <entity.h>
-#include <texture_loader.h>
+enum class ButtonsKeys : std::int32_t {
+  Entity,
+  Ability1,
+  Ability2,
+  Ability3,
+  Ability4,
+  EndTurn
+};
+
+class Window;
+class TextureLoader;
 
 class Board {
  public:
-  Board(TextureLoader& texture_loader, int cols, int rows)
-      : texture_loader_(texture_loader), cols_(cols), rows_(rows) {
-    for (int r = 0; r < rows_; ++r) {
-      for (int c = 0; c < cols_; ++c) {
-        fields_.emplace_back(texture_loader_.GetTexture("grass")).SetPos(c * 60, r * 60);
-      }
-    }
-  }
+  Board(TextureLoader& texture_loader, int cols, int rows);
 
-  void Draw(Window& window) {
-    for (auto& field : fields_) {
-      field.Draw(window);
-    }
-  }
+  void Draw(Window& window);
 
   // TODO: decide if we want to test it here?
-  [[nodiscard]] bool Clicked(int x, int y) const {
-    return x >= 0 && x <= cols_ * 60 && y >= 0 && y <= rows_ * 60;
-  }
+  [[nodiscard]] bool Clicked(int x, int y) const;
 
-  void ChangeTexture(int x, int y, const std::string& texture_key) {
-    ChangeTexture(ToIndex(x, y), texture_key);
-  }
+  void ChangeTexture(int x, int y, const std::string& texture_key);
 
-  void ChangeTexture(IndexType index, const std::string& texture_key) {
-    auto [x, y] = TextureAt(index).GetPos();
-    TextureAt(index) = texture_loader_.GetTexture(texture_key);
-    TextureAt(index).SetPos(x, y);
-  }
+  void ChangeTexture(IndexType index, const std::string& texture_key);
 
-  IndexType ToIndex(int x, int y) const {
-    return y * cols_ + x;
-  }
+  IndexType ToIndex(int x, int y) const;
 
-  void InteractPress(int x, int y) {
-    if (Clicked(x, y)) {
-      // pos to index
-
-    }
-  }
+  void InteractPress(int x, int y);
 
   template <typename Callback>
   void ForEachField(Callback callback) {
@@ -57,13 +42,7 @@ class Board {
   }
 
  private:
-  Texture& TextureAt(IndexType index) {
-    try {
-      return fields_.at(index);
-    } catch (std::out_of_range&) {
-      throw std::out_of_range("Board::TextureAt: index out of range: " + std::to_string(index));
-    }
-  }
+  Texture& TextureAt(IndexType index);
 
   TextureLoader& texture_loader_;
   int cols_;
